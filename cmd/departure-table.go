7ab184bbea1d66454cb7de8eae7b8024f76c766e
Kaya-Sem <kayasemvc@gmail.com>
2024-08-16 16:39:21 +0200

    implemented showing message after closing table with selection

--- cmd/departure-table.go
+++ cmd/departure-table.go
@@ -9,12 +9,21 @@ import (
 	"github.com/charmbracelet/lipgloss"
 )
 
+const (
+	BorderColor        = "240" // gray
+	SelectedForeground = "229" // not setting it to yellow will make the text yellow
+	SelectedBackground = "57"  // blue
+	tableHeight        = 6
+)
+
 var baseStyle1 = lipgloss.NewStyle().
 	BorderForeground(lipgloss.Color("9"))
 
 type tableModel struct {
 	table        table.Model
 	relativeTime string
+	showMessage  bool
+	message      string
 }
 
 func (m tableModel) Init() tea.Cmd { return nil }
@@ -27,7 +36,8 @@ func (m tableModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		case "q", "ctrl+c":
 			return m, tea.Quit
 		case "enter":
-			fmt.Println("\nconnection info...\n")
+			m.showMessage = true
+			m.message = "\n\nConnection info...\ntest\ntest\ntest"
 			return m, tea.Quit
 		}
 	}
@@ -48,6 +58,11 @@ func (m tableModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m tableModel) View() string {
+	if m.showMessage {
+		// Show the message instead of the table if the flag is set
+		return baseStyle1.Render(m.message)
+	}
+
 	// Add the relative time to the view only if there is a selected row
 	if m.relativeTime != "" {
 		return baseStyle1.Render(m.table.View()) + "\n\n" + "Departure in: " + m.relativeTime + "\n"
@@ -65,23 +80,21 @@ func RenderTable(columnItems []table.Column, rowItems []table.Row) {
 		table.WithColumns(columns),
 		table.WithRows(rows),
 		table.WithFocused(true),
-		table.WithHeight(6),
+		table.WithHeight(tableHeight),
 	)
 
-	// TODO: don't hardcode colors
 	s := table.DefaultStyles()
-	//	s.Cell.Align(lipgloss.Position(4))
 	s.Header = s.Header.
 		BorderStyle(lipgloss.NormalBorder()).
-		BorderForeground(lipgloss.Color("240")).
+		BorderForeground(lipgloss.Color(BorderColor)).
 		BorderBottom(true).
 		Bold(false)
 	s.Selected = s.Selected.
-		Foreground(lipgloss.Color("229")).
-		Background(lipgloss.Color("57"))
+		Foreground(lipgloss.Color(SelectedForeground)).
+		Background(lipgloss.Color(SelectedBackground))
 	t.SetStyles(s)
 
-	m := tableModel{t, ""}
+	m := tableModel{t, "", false, ""} // Initialize showMessage and message fields
 	if _, err := tea.NewProgram(m).Run(); err != nil {
 		fmt.Println("Error running program:", err)
 		os.Exit(1)
