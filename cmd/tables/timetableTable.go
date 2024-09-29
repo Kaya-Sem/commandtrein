b7e1b60a9dad0dd5b82449c1035404b26f282b1f
Kaya-Sem <kayasemvc@gmail.com>
2024-09-29 18:06:41 +0200

    removed details from timetable

--- cmd/tables/timetableTable.go
+++ cmd/tables/timetableTable.go
@@ -1,24 +1,23 @@
 package table
 
 import (
-	"fmt"
-	"github.com/Kaya-Sem/commandtrein/cmd"
 	"github.com/Kaya-Sem/commandtrein/cmd/api"
 )
 
 func getDetailedDepartureInfo(d api.TimetableDeparture) string {
-	relativeTime := CalculateHumanRelativeTime(d)
-	return fmt.Sprintf(`
-	Departure in: %s
-	Track: %s
-	Departure Time: %s
-	Vehicle: %s
-	Occupancy: %s
-`,
-		relativeTime,
-		d.Platform,
-		cmd.UnixToHHMM(d.Time),
-		d.Vehicle,
-		styleOccupancy(d.Occupancy.Name),
-	)
+	// 	relativeTime := CalculateHumanRelativeTime(d)
+	// 	return fmt.Sprintf(`
+	// 	Departure in: %s
+	// 	Track: %s
+	// 	Departure Time: %s
+	// 	Vehicle: %s
+	// 	Occupancy: %s
+	// `,
+	// 		relativeTime,
+	// 		d.Platform,
+	// 		cmd.UnixToHHMM(d.Time),
+	// 		d.Vehicle,
+	// 		styleOccupancy(d.Occupancy.Name),
+	// 	)
+	return ""
 }
