2605c89d2c7b239bcce63ad8d3564b4dc2f3dc15
Bram Windey <windey.bram@gmail.com>
2024-10-06 16:15:49 +0200

    Update bash completion to remove older caches

--- completions/bash_completion.sh
+++ completions/bash_completion.sh
@@ -5,12 +5,14 @@
 # $3 = word before word being completed
 
 _commandtrein(){
-  # Use a cache that will update every week
+	# Use a cache that will update every week
 	file="$HOME/.cache/commandtrein/$(date +'%m-%Y').txt"
 
 	if ! [ -f "$file" ]; then 
 		mkdir -p "$HOME/.cache/commandtrein/"
-    # Assumes that the binary is called commandtrein
+		# Remove older caches
+		rm "$HOME/.cache/commandtrein/*.txt"
+		# Assumes that the binary is called commandtrein
 		commandtrein search > "$file"
 	fi
 	mapfile -t COMPREPLY < <(grep "$2" "$file")
