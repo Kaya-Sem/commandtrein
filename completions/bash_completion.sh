a4a4545953ed3db83d4bbb0ae3f426834dcfb6f9
Bram Windey <windey.bram@gmail.com>
2024-10-06 16:27:16 +0200

    Update completions to support XDG-spec, and suppress 'rm' errors when no files were found

--- completions/bash_completion.sh
+++ completions/bash_completion.sh
@@ -6,12 +6,13 @@
 
 _commandtrein(){
 	# Use a cache that will update every week
-	file="$HOME/.cache/commandtrein/$(date +'%m-%Y').txt"
+	cache_dir="${XDG_CACHE_DIR:-$HOME/.cache}/commandtrein"
+	file="${cache_dir}/$(date +'%m-%Y').txt"
 
 	if ! [ -f "$file" ]; then 
-		mkdir -p "$HOME/.cache/commandtrein/"
+		mkdir -p "${cache_dir}"
 		# Remove older caches
-		rm "$HOME/.cache/commandtrein/*.txt"
+		rm "${cache_dir}/*.txt" 2>/dev/null
 		# Assumes that the binary is called commandtrein
 		commandtrein search > "$file"
 	fi
