376d3c060b1ef64c9c05e515212c8cdc8416c1cc
Kaya-Sem <kayasemvc@gmail.com>
2024-12-05 14:09:57 +0100

    fix infinite recursion in execution

---
+++ cmd/connection.go
@@ -0,0 +1,52 @@
+package cmd
+
+import (
+	"time"
+
+	"github.com/Kaya-Sem/commandtrein/api"
+	"github.com/Kaya-Sem/commandtrein/internal/util"
+	table "github.com/Kaya-Sem/commandtrein/tables"
+	teaTable "github.com/charmbracelet/bubbles/table"
+)
+
+func handleConnection(stationFrom string, stationTo string) {
+	s := NewSpinner("", " fetching connections", 1*time.Second)
+	s.Start()
+
+	connectionsJSON, err := api.GetConnections(stationFrom, stationTo)
+	if err != nil {
+		panic(err)
+	}
+
+	connections, err := api.ParseConnections(connectionsJSON)
+	if err != nil {
+		panic(err)
+	}
+
+	columns := []teaTable.Column{
+		{Title: "D", Width: 9},
+		{Title: "Duur", Width: 7},
+		{Title: "A", Width: 7},
+		{Title: "Spoor", Width: 10},
+	}
+
+	rows := make([]teaTable.Row, len(connections))
+
+	for i, conn := range connections {
+		departureTimeWithDelay := util.UnixToHHMM(conn.Departure.Time)
+		delay := util.FormatDelay(conn.Departure.Delay)
+		if delay != "" {
+			departureTimeWithDelay += " " + delay
+		}
+
+		rows[i] = teaTable.Row{
+			departureTimeWithDelay,
+			api.GetDurationInMinutes(conn),
+			util.UnixToHHMM(conn.Arrival.Time),
+			conn.Departure.Platform,
+		}
+	}
+
+	s.Stop()
+	table.RenderTable(columns, rows, connections)
+}
--- cmd/root.go
+++ cmd/root.go
@@ -3,73 +3,50 @@ package cmd
 import (
 	"fmt"
 	"os"
-	"time"
 
-	"github.com/Kaya-Sem/commandtrein/api"
-	"github.com/Kaya-Sem/commandtrein/internal/util"
-	table "github.com/Kaya-Sem/commandtrein/tables"
-	teaTable "github.com/charmbracelet/bubbles/table"
 	"github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{
 	Use:   "commandtrein [shortcut|station1] [station2]",
 	Short: "commandtrein helps you find train connections in Belgium",
-	Long:  ``,
+	Long: `commandtrein is a CLI tool for checking train schedules and connections in Belgium.
+You can use it with station names directly or configure shortcuts for frequent routes.`,
+	Args: cobra.MaximumNArgs(2),
 	Run: func(cmd *cobra.Command, args []string) {
-		// Do Stuff Here
+		switch len(args) {
+		case 0:
+			cmd.Help()
+		case 1:
+			// Check if argument is a shortcut
+			if shortcut, exists := GetShortcut(args[0]); exists {
+				handleConnection(shortcut.Station1, shortcut.Station2)
+			} else {
+				// Check if it's a valid subcommand
+				found := false
+				for _, c := range cmd.Root().Commands() {
+					if c.Name() == args[0] {
+						found = true
+						fmt.Printf("Please use: commandtrein %s --help for more information\n", args[0])
+						break
+					}
+				}
+				if !found {
+					fmt.Println("\nNo such command or shortcut found!")
+					cmd.Help()
+				}
+			}
+			os.Exit(1)
+		case 2:
+			handleConnection(args[0], args[1])
+		}
 	},
 }
 
-func init() {
-	cobra.OnInitialize(initConfig)
-}
-
 func Execute() {
+	initConfig()
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
-}
-
-func handleConnection(stationFrom string, stationTo string) {
-	s := NewSpinner("", " fetching connections", 1*time.Second)
-	s.Start()
-
-	connectionsJSON, err := api.GetConnections(stationFrom, stationTo)
-	if err != nil {
-		panic(err)
-	}
-
-	connections, err := api.ParseConnections(connectionsJSON)
-	if err != nil {
-		panic(err)
-	}
-
-	columns := []teaTable.Column{
-		{Title: "D", Width: 9},
-		{Title: "Duur", Width: 7},
-		{Title: "A", Width: 7},
-		{Title: "Spoor", Width: 10},
-	}
-
-	rows := make([]teaTable.Row, len(connections))
-
-	for i, conn := range connections {
-		departureTimeWithDelay := util.UnixToHHMM(conn.Departure.Time)
-		delay := util.FormatDelay(conn.Departure.Delay)
-		if delay != "" {
-			departureTimeWithDelay += " " + delay
-		}
-
-		rows[i] = teaTable.Row{
-			departureTimeWithDelay,
-			api.GetDurationInMinutes(conn),
-			util.UnixToHHMM(conn.Arrival.Time),
-			conn.Departure.Platform,
-		}
-	}
-
-	s.Stop()
-	table.RenderTable(columns, rows, connections)
 }
